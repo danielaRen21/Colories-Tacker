6199680bb921e9b6b6cb50f53b6e6211379d8273
danielaRen21 <daniela.f.renteria@gmail.com>
2026-01-12 10:39:26 -0600

    First commit

---
+++ src/components/Form.tsx
@@ -0,0 +1,64 @@
+/** @format */
+
+import { categories } from "../data/categories";
+
+export default function Form() {
+  return (
+    <form className='space-y5 bg-white shadow p-10 rounded-lg'>
+      <div className='grid grid-cols-1 gap-3'>
+        <label
+          htmlFor='category'
+          className='font-bold'
+        >
+          Categorias:
+        </label>
+        <select
+          className='border border-slate-300 p-2 rounded-lg w-full bg-white'
+          id='category'
+        >
+          {categories.map((category) => (
+            <option
+              key={category.id}
+              value={category.id}
+            >
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className='grid grid-cols-1 gap-3'>
+        <label
+          htmlFor='activity'
+          className='font-bold'
+        >
+          Actividad:
+        </label>
+        <input
+          type='text'
+          id='activity'
+          className='border border-slate-300 p-2 rounded-lg'
+          placeholder='Ej. Comida, Jugo, Pesas, Bicicleta'
+        />
+      </div>
+      <div className='grid grid-cols-1 gap-3'>
+        <label
+          htmlFor='calories'
+          className='font-bold'
+        >
+          Calorias:
+        </label>
+        <input
+          type='number'
+          id='calories'
+          className='border border-slate-300 p-2 rounded-lg'
+          placeholder='Calorias, ej. 300 o 500'
+        />
+      </div>
+      <input
+        type='submit'
+        className='bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer'
+        value='GUARDAR COMIDA O GUARDAR EJERCICIO'
+      />
+    </form>
+  );
+}
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+/** @format */
+
+export type Category = {
+  id: number;
+  name: string;
+};
